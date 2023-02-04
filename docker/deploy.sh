41a382b5c711971a3dbbaa0474d85679b54ee22a
Jean28518 <jf_v@gmx.de>
2023-02-04 14:51:05 +0100

    finish docker deploy

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,7 +1,9 @@
-ln -s ../application ./
-ln -s ../webserver ./
-ln -s ../database ./sql/
-ln -s ../images/muscari-database.Containerfile ./sql/dockerfile
-ln -s ../images/muscari-webserver.Containerfile ./dockerfile
+mkdir sql
+
+cp -r ../application ./
+cp -r ../webserver ./
+cp -r ../database ./sql/
+cp ../images/muscari-database.Containerfile ./sql/Dockerfile
+cp ../images/muscari-webserver.Containerfile ./Dockerfile
 
 docker-compose up -d --build
